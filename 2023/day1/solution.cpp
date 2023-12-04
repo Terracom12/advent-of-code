40bb8c3b41a89757bc8627fa9f7a2a9cb6438d47
Matthew Reese <awesomepop012@hotmail.com>
2023-12-04 11:18:19 -0800

    Refactored day 1 code

--- 2023/day1/solution.cpp
+++ 2023/day1/solution.cpp
@@ -1,38 +1,78 @@
 #include "../../utils.hpp"
 
-int main() {
-    using namespace std;
-    ifstream file;
-    file.open("input.txt");
+int part1(std::vector<std::string> input) {
+    int firstDigit, lastDigit, number, sum = 0;
 
-    string line;
-    int n1, n2, n, sum = 0;
-
-    vector<string> numberWords = 
-        { "one", "two", "three", "four", "five", "six", 
-          "seven", "eight", "nine" };
-
-    while(getline(file, line)) {
-        n1 = n2 = 0;
+    for(auto line : input) {
+        firstDigit = lastDigit = 0;
         for(int i = 0; i < line.size(); ++i) {
             char c = line[i];
-            if(!isalpha(c)) {
-                n2 = atoi(&c);
+            if(isdigit(c)) {
+                lastDigit = c - '0';
+            }
+
+            if(!firstDigit) {
+                firstDigit = lastDigit;
+            }
+        }
+
+        number = firstDigit * 10 + lastDigit;
+        sum += number;
+    }
+
+    return sum;
+}
+
+int part2(std::vector<std::string> input) {
+    std::vector<std::string> numToWord = 
+        { "one", "two", "three", "four", "five", "six", 
+          "seven", "eight", "nine" };
+    int firstDigit, lastDigit, number, sum = 0;
+
+    for(auto& line : input) {
+        firstDigit = lastDigit = 0;
+        for(int i = 0; i < line.size(); i++) {
+            char c = line[i];
+            if(isdigit(c)) {
+                lastDigit = c - '0';
             } else {
-                for(int j = 0; j < numberWords.size(); ++j) {
-                    if(line.substr(i, numberWords[j].size()) == numberWords[j])
-                        n2 = j + 1;
+                for(int j = 0; j < numToWord.size(); ++j) {
+                    if(line.substr(i, numToWord[j].size()) == numToWord[j]) {
+                        lastDigit = j + 1;
+                    }
                 }
             }
-            if(!n1)
-                n1 = n2;
-        }
-        // cout << "1: " << n1 << " 2: "<< n2 << endl;
-        n = n1 * 10 + n2;
-        sum += n;
 
+            if(!firstDigit) {
+                firstDigit = lastDigit;
+            }
+        }
+
+        number = firstDigit * 10 + lastDigit;
+        sum += number;
+    }
+
+    return sum;
+}
+
+int main() {
+    std::ifstream file;
+    file.open("input.txt");
+
+    if(!file) {
+        std::cout << "ERROR: Could not open file 'input.txt'!\n";
+        return -1;
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+    int firstDigit, lastDigit, n, sum = 0;
+
+    while(getline(file, line)) {
+        lines.push_back(line);
     }
     file.close();
 
-    std::cout << sum;
+    std::cout << "Part 1: " << part1(lines) << std::endl;
+    std::cout << "Part 2: " << part2(lines) << std::endl;
 }
