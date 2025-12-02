a8dfa709a7876c1d529d1cb8b2617a54b1db69a1
Matthew Reese <awesomepop012@hotmail.com>
2025-12-01 19:41:40 -0800

    create basic template system for new days

---
+++ templates/sol.py
@@ -0,0 +1,14 @@
+from aoc_util import AOCBase, aoc_run
+
+class Day{{DAY}}(AOCBase):
+    # def process_input(self, raw_input: str) -> list[str]:
+    #     return raw_input.split('\n')
+
+    def part1(self, inp: str) -> int:
+        return -1
+
+    # def part2(self, inp: str) -> int:
+    #     return -1
+
+if __name__ == "__main__":
+    aoc_run({{YEAR}}, {{DAY}}, Day{{DAY}})
